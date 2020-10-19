199e80dce34ab6952c5c2ac1d0089de48e21fdbf
”Saniok2003” <sasha06ura@gmail.com>
2020-10-19 12:16:11 +0300

    First commit

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("O My GoD YOU CRAZZY");
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random rnd = new Random();
+            button2.Location = new Point(button2.Location.X + rnd.Next(-15, 15), button2.Location.Y + rnd.Next(-15, 15));
+        }
+    }
+}
